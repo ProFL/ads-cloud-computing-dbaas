66616e78446ed9387f11a353ca7604162ccb1f9a
Pedro F Linhares <pedro@profl.com.br>
2021-06-01 02:06:25 -0300

    :sparkles: Listagem e criação de usuários

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const db = require("../firebase/firestore");
+
+router.post("/", function (req, res, next) {
+  const data = req.body;
+
+  const ref = db.collection("users").doc();
+  ref
+    .set(data)
+    .then((v) => {
+      console.dir(v);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/", function (req, res, next) {
+  db.collection("users")
+    .get()
+    .then((usersRef) => {
+      const users = [];
+      usersRef.forEach((ur) => {
+        users.push({ id: ur.id, ...ur.data() });
+      });
+      res.render("users", { users });
+    })
+    .catch(next);
 });
 
 module.exports = router;
